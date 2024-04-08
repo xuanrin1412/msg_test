a495873e14527198e1aaaee679fb5ea4e987cf1f
xuanrin1412 <xuanrin1412@gmail.com>
2024-04-08 20:21:42 +0700

    update msg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use client"
 import type { Metadata } from "next";
 import { Roboto, Lobster } from "next/font/google";
 import "./globals.css";
--- src/components/faceBookMsg.tsx
+++ src/components/faceBookMsg.tsx
@@ -1,14 +1,16 @@
-"use client"
-import React, { Component } from 'react';
+import React from 'react';
 import { FacebookProvider, CustomChat } from 'react-facebook';
 
 function FaceBookMsg() {
-    return <FacebookProvider appId="371945669163262" chatSupport>
-        <CustomChat pageId="288387651020716" minimized={true} />
-    </FacebookProvider>
+    if (typeof window !== 'undefined') {
+        return (
+            <FacebookProvider appId="371945669163262" chatSupport>
+                <CustomChat pageId="288387651020716" minimized={true} />
+            </FacebookProvider>
+        );
+    } else {
+        return null;
+    }
 }
 
 export default FaceBookMsg;
-
-
-
